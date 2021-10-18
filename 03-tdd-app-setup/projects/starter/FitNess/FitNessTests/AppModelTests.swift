432b78b19a0e084df36f23546ad3e8490115d45c
hikeuchi <hikeuchi@morisawa.co.jp>
2021-10-18 15:27:02 +0900

    until "Your first test"

---
+++ 03-tdd-app-setup/projects/starter/FitNess/FitNessTests/AppModelTests.swift
@@ -0,0 +1,21 @@
+//
+//  AppModelTests.swift
+//  FitNessTests
+//
+//  Created by HIROKI IKEUCHI on 2021/10/18.
+//  Copyright © 2021 Razeware. All rights reserved.
+//
+
+import XCTest
+
+class AppModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+}
